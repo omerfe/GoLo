9e77981e5f42afd524be8cbe3bf7637e4048c43f
sedaahi <ahiseda@gmail.com>
2022-03-05 13:55:14 +0300

    Checkout

--- src/Web/Controllers/HomeController.cs
+++ src/Web/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace Web.Controllers
         {
             return View();
         }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
