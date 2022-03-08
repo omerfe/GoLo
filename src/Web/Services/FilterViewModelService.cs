cc3806dbf9bd431288ed8f026e16e6a32134a4f2
MAliRagipCakir <m-ar-cakir@hotmail.com>
2022-03-08 17:03:10 +0300

    FilterPagePagination

--- src/Web/Services/FilterViewModelService.cs
+++ src/Web/Services/FilterViewModelService.cs
@@ -1,7 +1,9 @@
 ﻿using ApplicationCore.Entities;
 using ApplicationCore.Interfaces;
 using ApplicationCore.Specifications;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,43 +18,55 @@ namespace Web.Services
         private readonly IRepository<Genre> _genreRepo;
         private readonly IRepository<Platform> _platformRepo;
         private readonly IRepository<Discount> _discountRepo;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public FilterViewModelService(IRepository<Product> productRepo, IRepository<Genre> genreRepo, IRepository<Platform> platformRepo, IRepository<Discount> discountRepo)
+        public FilterViewModelService(IRepository<Product> productRepo, IRepository<Genre> genreRepo, IRepository<Platform> platformRepo, IRepository<Discount> discountRepo, IHttpContextAccessor httpContextAccessor)
         {
             _productRepo = productRepo;
             _genreRepo = genreRepo;
             _platformRepo = platformRepo;
             _discountRepo = discountRepo;
+            _httpContextAccessor = httpContextAccessor;
         }
 
-        public async Task<FilterViewModel> GetFilterViewModelAsync(int? genreId, int? platformId)
+        //public async Task<FilterViewModel> GetFilterViewModelAsync(int? genreId, int? platformId)
+        public async Task<FilterViewModel> GetFilterViewModelAsync(List<int> genreIds, List<int> platformIds, int page)
         {
-            var specProducts = new ProductsFilterSpecification(genreId, platformId);
+
+            var specAllProducts = new ProductsFilterSpecification(genreIds, platformIds);
+            int totalItems = await _productRepo.CountAsync(specAllProducts);
+            int totalPages = (int)Math.Ceiling((double)totalItems / Constants.ITEMS_PER_PAGE);
+            var specProducts = new ProductsFilterSpecification(genreIds, platformIds, (page - 1) * Constants.ITEMS_PER_PAGE, Constants.ITEMS_PER_PAGE);
+
             List<Product> products = await _productRepo.GetAllAsync(specProducts);
 
             List<Genre> genres = await _genreRepo.GetAllAsync();
             List<Platform> platforms = await _platformRepo.GetAllAsync();
 
-            //var specDiscounts = await _discountRepo.GetAllAsync(new DiscountSpesification(x.Id))[0]
-
-            var discountRate = await _discountRepo.GetAllAsync();
-
-            FilterViewModel vm = new FilterViewModel()
+            FilterViewModel vm = new FilterViewModel(_httpContextAccessor)
             {
                 Products = products.Select(x => new ProductViewModel()
                 {
-
                     Id = x.Id,
                     GameName = x.Game.GameName,
                     UnitPrice = x.ProductUnitPrice,
                     PicturePath = x.Game.ImagePath,
                     PlatformLogo = x.Platform.LogoPath,
-                    DiscountRate = x.Discounts.FirstOrDefault(x => x.IsValid) == null ? 0 : x.Discounts.FirstOrDefault(x => x.IsValid).DiscountRate
+                    DiscountRate = x.Discounts.FirstOrDefault(x => x.IsValid) == null ? 0 : x.Discounts.FirstOrDefault(x => x.IsValid).DiscountRate//TODO Test edilmedi,discount'lu game yok
                 }).ToList(),
                 Genres = genres.Select(x => new SelectListItem(x.GenreName, x.Id.ToString())).ToList(),
                 Platforms = platforms.Select(x => new SelectListItem(x.PlatformName, x.Id.ToString())).ToList(),
-                GenreId = genreId,
-                PlatformId = platformId
+                GenreIds = genreIds,
+                PlatformIds = platformIds,
+                PaginationInfo = new PaginationInfoViewModel()
+                {
+                    CurrentPage = page,
+                    ItemsOnPage = products.Count,
+                    TotalItems = totalItems,
+                    TotalPages = totalPages,
+                    HasPrevious = page > 1,
+                    HasNext = page < totalPages
+                }
             };
 
             return vm;
