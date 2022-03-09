b078139bb876d48dc9540608bb9d7d19092a433d
omerfe <omerfe.eker@gmail.com>
2022-03-09 17:03:23 +0300

    Hover

--- src/Web/wwwroot/js/multiselect-dropdown.js
+++ src/Web/wwwroot/js/multiselect-dropdown.js
@@ -5,7 +5,7 @@ style.innerHTML = `
   display: inline-block;
   padding: 2px 5px 0px 5px;
   border-radius: 4px;
-  border: solid 1px #ced4da;
+  border: solid 1px #61528B;
   position: relative;
   background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='%23343a40' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M2 5l6 6 6-6'/%3e%3c/svg%3e");
   background-repeat: no-repeat;
