e725ad069b2998f325110dfe34e5debd2e20111a
omerfe <omerfe.eker@gmail.com>
2022-03-10 19:30:23 +0300

    AuthorizationConstants, GoloIdentityDbContextSeed, Login-Register Pages with Views

---
+++ src/ApplicationCore/Constants/AuthorizationConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace ApplicationCore.Constants
+{
+	public static class AuthorizationConstants
+	{
+		public const string DEFAULT_PASSWORD = "P@ssword1";
+		public static class Roles
+		{
+			public const string ADMIN = "admin";
+		}
+	}
+}
