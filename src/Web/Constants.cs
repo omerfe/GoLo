134013beb28d11d05fa968f843a1ac7ccd280cc6
omerfe <omerfe.eker@gmail.com>
2022-03-12 16:22:54 +0300

    Navbar, HomeViewFixed, FilterPageLink

--- src/Web/Constants.cs
+++ src/Web/Constants.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Web
+﻿namespace Web
 {
     public static class Constants
     {
-        public const int ITEMS_PER_PAGE = 2;
+        public const int ITEMS_PER_PAGE = 20;
         public const string CART_COOKIENAME = "BuyerId";
     }
 }
