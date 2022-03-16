60be2d5c9fd406b2b886da09a91db574450d229b
MAliRagipCakir <m-ar-cakir@hotmail.com>
2022-03-16 22:56:06 +0300

    Admin Genre Crud fixes Admin Platform Crud

---
+++ src/Web/Areas/Admin/Models/Validators/PlatformEditViewModelValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Areas.Admin.Models.Validators
+{
+    public class PlatformEditViewModelValidator : AbstractValidator<PlatformEditViewModel>
+    {
+        public PlatformEditViewModelValidator()
+        {
+            RuleFor(x => x.PlatformName)
+                    .NotNull().WithMessage("This field is required.")
+                    .NotEmpty().WithMessage("This field is required.")
+                    .MaximumLength(100).WithMessage("This field has max length of {0}");
+
+            RuleFor(x => x.LogoPath)
+                    .MaximumLength(100).WithMessage("This field has max length of {0}");
+
+            RuleFor(x => x.LogoImage)
+                    .SetValidator(new FileValidator(2));
+        }
+    }
+}
---
+++ src/Web/Areas/Admin/Models/Validators/PlatformViewModelValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Areas.Admin.Models.Validators
+{
+    public class PlatformViewModelValidator : AbstractValidator<PlatformViewModel>
+    {
+        public PlatformViewModelValidator()
+        {
+            RuleFor(x => x.PlatformName)
+                .NotNull().WithMessage("This field is required.")
+                .NotEmpty().WithMessage("This field is required.")
+                .MaximumLength(100).WithMessage("This field has max length of {0}");
+
+            RuleFor(x => x.LogoPath)
+                .MaximumLength(100).WithMessage("This field has max length of {0}");
+
+            RuleFor(x => x.LogoImage)
+                .NotNull().WithMessage("This field is required.")
+                .NotEmpty().WithMessage("This field is required.")
+                .SetValidator(new FileValidator(2));
+        }
+    }
+}
