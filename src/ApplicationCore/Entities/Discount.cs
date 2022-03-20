27ed1ac009cfc998d49dc1ded383259b579d7789
MAliRagipCakir <m-ar-cakir@hotmail.com>
2022-03-20 16:51:51 +0300

    Discount date overlap fixed & IsValid prop fixed

--- src/ApplicationCore/Entities/Discount.cs
+++ src/ApplicationCore/Entities/Discount.cs
@@ -7,7 +7,7 @@ namespace ApplicationCore.Entities
         public int DiscountRate { get; set; }
         public DateTime ValidFrom { get; set; }
         public DateTime ValidUntil { get; set; }
-        public bool IsValid => (ValidUntil >= DateTime.Now && ValidFrom <= DateTime.Now);
+        public bool IsValid => (ValidUntil.Date >= DateTime.Now.Date && ValidFrom.Date <= DateTime.Now.Date);
         public int ProductId { get; set; }
         public Product Product { get; set; }
     }
