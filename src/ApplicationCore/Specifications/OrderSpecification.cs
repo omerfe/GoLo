2a63358cac9908d9c74d6428f205232bd2e11156
sinaemre <snabkr7010@gmail.com>
2022-03-22 20:52:48 +0300

    Checkout

---
+++ src/ApplicationCore/Specifications/OrderSpecification.cs
@@ -0,0 +1,27 @@
+﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Specifications
+{
+    public class OrderSpecification : Specification<Order>
+    {
+        public OrderSpecification(string buyerId)
+        {
+            Query.Where(x => x.BuyerId == buyerId)
+                .Include(x => x.OrderDetails)
+                .ThenInclude(x => x.Key)
+                .ThenInclude(x => x.Product)
+                .ThenInclude(x => x.Game);
+            Query.Where(x => x.BuyerId == buyerId)
+                .Include(x => x.OrderDetails)
+                .ThenInclude(x => x.Key)
+                .ThenInclude(x => x.Product)
+                .ThenInclude(x => x.Platform);
+        }
+    }
+}
